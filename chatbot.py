416708b19c613b868a5e1bded9ffc191b41b1fcf
LuCkY <hkevin090602@gmail.com>
2022-07-15 19:53:13 -0400

    Text To Speech Component

--- chatbot.py
+++ chatbot.py
@@ -1,18 +1,25 @@
 import os
 from wolframalpha import Client
 from decouple import config
+import pyttsx3
 
 API_KEY = config('KEY')
-
 client = Client(API_KEY) # API Key
 
+#Text to Speech
+engine = pyttsx3.init()
+engine.setProperty("rate", 200)
+engine.say("What is your question?")
+engine.runAndWait()
+
+#Search Engine Section
 question = input("What is your question?")
-
 response = client.query(question)
-
 pod = next(response.results)
-
 answer = pod.text
 
+#Text to Speech
 print("Your question: {}".format(question))
+engine.say(answer)
+engine.runAndWait()
 print("Answer: {}".format(answer))
