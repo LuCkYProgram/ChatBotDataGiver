a4a14125e706b7e18258b66e302040ac912f4a91
LuCkY <hkevin090602@gmail.com>
2022-07-22 20:26:24 -0400

    Google Search Engine

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,7 @@ from wolframalpha import Client
 from decouple import config
 import pyttsx3
 import speech_recognition
+from googlesearch import search
 
 API_KEY = config('KEY')
 client = Client(API_KEY) # API Key
@@ -13,7 +14,8 @@ voices = engine.getProperty("voices")
 voice_id_to_use = voices[1].id
 engine.setProperty("voice", voice_id_to_use)
 engine.setProperty("rate", 200)
-engine.say("What is your question?")
+
+engine.say("What would you like to do? Say or type MATH or GOOGLE.")
 engine.runAndWait()
 
 #Speech To Text
@@ -24,18 +26,44 @@ with microphone as source:
 with microphone as source:
     print("Listening...")
     audio = recognizer.listen(source)
-question = recognizer.recognize_google(audio)
-print("You said: " + question)
+choice = recognizer.recognize_google(audio)
+print("You said: " + choice)
 
 #Search Engine Section
 #question = input("What is your question?")
-if question == "covid-19":
-    answer = "Here is the most updated website by John Hopkins."
-    print(answer)
-    engine.say(answer)
+
+# if question == "covid-19":
+#     answer = "Here is the most updated website by John Hopkins."
+#     print(answer)
+#     engine.say(answer)
+#     engine.runAndWait()
+#     print("https://coronavirus.jhu.edu/map.html")
+
+if choice == "Google":
+    engine.say("What is your query?")
     engine.runAndWait()
-    print("https://coronavirus.jhu.edu/map.html")
-else:
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+    with microphone as source:
+        print("Listening...")
+        audio = recognizer.listen(source)
+    query = recognizer.recognize_google(audio)
+    engine.say("You said: " + query)
+    engine.runAndWait()
+    engine.say("Here is your top 10 results of your query based by Google.")
+    engine.runAndWait()
+    print("Here is your top 10 results of your query based by Google.")
+    for j in search(query, tld="co.in", num=10, stop=10, pause=2):
+        print(j)
+elif choice == "math":
+    engine.say("What is your question?")
+    engine.runAndWait()
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+    with microphone as source:
+        print("Listening...")
+        audio = recognizer.listen(source)
+    question = recognizer.recognize_google(audio)
     response = client.query(question)
     pod = next(response.results)
     answer = pod.text
