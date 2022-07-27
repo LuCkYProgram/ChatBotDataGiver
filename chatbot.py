3b598d0a2cfe8f0d3fac37bfb9c124eb8d9c8c38
LuCkY <hkevin090602@gmail.com>
2022-07-26 20:09:22 -0400

    google dataset searcher

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,14 @@
 from logging.config import listen
 import os
-from wolframalpha import Client
 from decouple import config
+
 import pyttsx3
 import speech_recognition
+
+from wolframalpha import Client
 from googlesearch import search
+import requests
+from bs4 import BeautifulSoup
 
 API_KEY = config('KEY')
 client = Client(API_KEY) # API Key
@@ -19,7 +23,7 @@ voice_id_to_use = voices[1].id
 engine.setProperty("voice", voice_id_to_use)
 engine.setProperty("rate", 200)
 
-engine.say("What would you like to do? Say or type MATH or GOOGLE.")
+engine.say("What would you like to do? Say or type MATH or GOOGLE or DATASET.")
 engine.runAndWait()
 with microphone as source:
     recognizer.adjust_for_ambient_noise(source)
@@ -38,6 +42,23 @@ print("You said: " + choice)
 #     engine.say(answer)
 #     engine.runAndWait()
 #     print("https://coronavirus.jhu.edu/map.html")
+
+def data_google(data):
+    dataset = data
+    dataset_clean = dataset.replace(' ', '+').lower()
+    link = f'https://datasetsearch.research.google.com/search?query={dataset_clean}'
+
+    req = requests.get(link)
+    soup = BeautifulSoup(req.content, 'html.parser')
+
+    name = soup.find_all('h1',  class_="iKH1Bc")
+    site = soup.find_all('li', class_="iW1HZe")
+    print('\n\n--------------------DATASETS FOUND IN GOOGLE DATA SEARCH\n\n')
+    print(f'Link to access this and others datasets:{link}')
+    for i in range(5):
+        name_clean = str(name[i]).split('">')[1].split('</')[0]
+        site_clean = str(site[i]).split('">')[1].split('<')[0]
+    print(f'\n{name_clean}\n Dataset source: {site_clean}\n')
 
 def listening_replying_function(to_ask):
     engine.say(to_ask)
@@ -64,10 +85,12 @@ elif choice == "math":
     response = client.query(question)
     pod = next(response.results)
     answer = pod.text
-    #Text to Speech
     print("Your question: {}".format(question))
     engine.say("The answer is " + answer)
     engine.runAndWait()
     print("Answer: {}".format(answer))
+elif choice == "dataset":
+    key_word = listening_replying_function("What dataset do you want to find?")
+    data_google(key_word)
 
 
