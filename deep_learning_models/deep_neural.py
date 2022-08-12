e69d7cabd732e3e9a671ee8e7dde5f1695ecf185
LuCkY <hkevin090602@gmail.com>
2022-08-12 15:41:28 -0400

    prepare data for PyTorch

--- deep_learning_models/deep_neural.py
+++ deep_learning_models/deep_neural.py
@@ -6,3 +6,18 @@ import torch.utils.data as td
 torch.manual_seed(0)
 
 print("Libraries imported - ready to use PyTorch", torch.__version__)
+
+# Create a dataset and loader for the training data and labels
+train_x = torch.Tensor(x_train).float()
+train_y = torch.Tensor(y_train).long()
+train_ds = td.TensorDataset(train_x,train_y)
+train_loader = td.DataLoader(train_ds, batch_size=20,
+    shuffle=False, num_workers=1)
+
+# Create a dataset and loader for the test data and labels
+test_x = torch.Tensor(x_test).float()
+test_y = torch.Tensor(y_test).long()
+test_ds = td.TensorDataset(test_x,test_y)
+test_loader = td.DataLoader(test_ds, batch_size=20,
+    shuffle=False, num_workers=1)
+print('Ready to load data')
