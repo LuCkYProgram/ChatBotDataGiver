6b6ac8f1eb40e3d1415362811738f3f646370c5d
LuCkY <hkevin090602@gmail.com>
2022-08-11 21:05:16 -0400

    PyTorch

---
+++ deep_learning_models/deep_neural.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.utils.data as td
+
+# Set random seed for reproducability
+torch.manual_seed(0)
+
+print("Libraries imported - ready to use PyTorch", torch.__version__)
