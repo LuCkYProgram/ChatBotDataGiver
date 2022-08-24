fcf17316874d8d45a653633faf6b4e74e032ff04
LuCkY <hkevin090602@gmail.com>
2022-08-24 16:51:47 -0400

    Loading Convolutional

--- deep_learning_models/convolutional_neural.py
+++ deep_learning_models/convolutional_neural.py
@@ -35,3 +35,47 @@ for sub_dir in os.listdir(data_path):
     imgplot = plt.imshow(img)
     a.set_title(img_file)
 plt.show()
+
+def load_dataset(data_path):
+    # Load all of the images
+    transformation = transforms.Compose([
+        # transform to tensors
+        transforms.ToTensor(),
+        # Normalize the pixel values (in R, G, and B channels)
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+
+    # Load all of the images, transforming them
+    full_dataset = torchvision.datasets.ImageFolder(
+        root=data_path,
+        transform=transformation
+    )
+    
+    
+    # Split into training (70% and testing (30%) datasets)
+    train_size = int(0.7 * len(full_dataset))
+    test_size = len(full_dataset) - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, test_size])
+    
+    # define a loader for the training data we can iterate through in 50-image batches
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=50,
+        num_workers=0,
+        shuffle=False
+    )
+    
+    # define a loader for the testing data we can iterate through in 50-image batches
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=50,
+        num_workers=0,
+        shuffle=False
+    )
+        
+    return train_loader, test_loader
+
+
+# Get the iterative dataloaders for test and training data
+train_loader, test_loader = load_dataset(data_path)
+print('Data loaders ready')
