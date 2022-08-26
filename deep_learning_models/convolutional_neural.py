b123b58f2bd167e8982389436c4e551b8e315de5
LuCkY <hkevin090602@gmail.com>
2022-08-26 18:08:42 -0400

    Define CNN

--- deep_learning_models/convolutional_neural.py
+++ deep_learning_models/convolutional_neural.py
@@ -79,3 +79,49 @@ def load_dataset(data_path):
 # Get the iterative dataloaders for test and training data
 train_loader, test_loader = load_dataset(data_path)
 print('Data loaders ready')
+
+class Net(nn.Module):
+    # Constructor
+    def __init__(self, num_classes=3):
+        super(Net, self).__init__()
+        
+        # Our images are RGB, so input channels = 3. We'll apply 12 filters in the first convolutional layer
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=12, kernel_size=3, stride=1, padding=1)
+        
+        # We'll apply max pooling with a kernel size of 2
+        self.pool = nn.MaxPool2d(kernel_size=2)
+        
+        # A second convolutional layer takes 12 input channels, and generates 12 outputs
+        self.conv2 = nn.Conv2d(in_channels=12, out_channels=12, kernel_size=3, stride=1, padding=1)
+        
+        # A third convolutional layer takes 12 inputs and generates 24 outputs
+        self.conv3 = nn.Conv2d(in_channels=12, out_channels=24, kernel_size=3, stride=1, padding=1)
+        
+        # A drop layer deletes 20% of the features to help prevent overfitting
+        self.drop = nn.Dropout2d(p=0.2)
+        
+        # Our 128x128 image tensors will be pooled twice with a kernel size of 2. 128/2/2 is 32.
+        # So our feature tensors are now 32 x 32, and we've generated 24 of them
+        # We need to flatten these and feed them to a fully-connected layer
+        # to map them to  the probability for each class
+        self.fc = nn.Linear(in_features=32 * 32 * 24, out_features=num_classes)
+
+    def forward(self, x):
+        # Use a relu activation function after layer 1 (convolution 1 and pool)
+        x = F.relu(self.pool(self.conv1(x)))
+      
+        # Use a relu activation function after layer 2 (convolution 2 and pool)
+        x = F.relu(self.pool(self.conv2(x)))
+        
+        # Select some features to drop after the 3rd convolution to prevent overfitting
+        x = F.relu(self.drop(self.conv3(x)))
+        
+        # Only drop the features if this is a training pass
+        x = F.dropout(x, training=self.training)
+        
+        # Flatten
+        x = x.view(-1, 32 * 32 * 24)
+        # Feed to fully-connected layer to predict class
+        x = self.fc(x)
+        # Return log_softmax tensor 
+        return F.log_softmax(x, dim=1)
